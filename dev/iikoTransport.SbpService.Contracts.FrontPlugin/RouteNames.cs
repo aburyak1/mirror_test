c7fb3ffca88721483993aba64cd4335b4b1c4e36
Andrey Buryak <aburyak@iikosoftware.com>
2023-02-07 12:35:41 +0300

    TRN-1886 TRN-1887 buildfix

--- dev/iikoTransport.SbpService.Contracts.FrontPlugin/RouteNames.cs
+++ dev/iikoTransport.SbpService.Contracts.FrontPlugin/RouteNames.cs
@@ -1,57 +1,58 @@
-﻿namespace iikoTransport.SbpService.Contracts.FrontPlugin;
-
-/// <summary>
-/// Message routes.
-/// </summary>
-public class RouteNames
+﻿namespace iikoTransport.SbpService.Contracts.FrontPlugin
 {
     /// <summary>
-    /// Регистрация одноразовой Функциональной ссылки СБП для B2B.
+    /// Message routes.
     /// </summary>
-    public const string CreateOneTimePaymentLink = "PluginsSbp/CreateOneTimePaymentLink";
+    public class RouteNames
+    {
+        /// <summary>
+        /// Регистрация одноразовой Функциональной ссылки СБП для B2B.
+        /// </summary>
+        public const string CreateOneTimePaymentLink = "PluginsSbp/CreateOneTimePaymentLink";
 
-    /// <summary>
-    /// Регистрация многоразовой Функциональной ссылки СБП для B2B.
-    /// </summary>
-    public const string CreateReusablePaymentLink = "PluginsSbp/CreateReusablePaymentLink";
+        /// <summary>
+        /// Регистрация многоразовой Функциональной ссылки СБП для B2B.
+        /// </summary>
+        public const string CreateReusablePaymentLink = "PluginsSbp/CreateReusablePaymentLink";
 
-    /// <summary>
-    /// Запрос содержимого для ранее зарегистрированной Функциональной ссылки СБП.
-    /// </summary>
-    public const string GetQrcPayload = "PluginsSbp/GetQrcPayload";
+        /// <summary>
+        /// Запрос содержимого для ранее зарегистрированной Функциональной ссылки СБП.
+        /// </summary>
+        public const string GetQrcPayload = "PluginsSbp/GetQrcPayload";
 
-    /// <summary>
-    /// Получение идентификаторов для многоразовых ссылок СБП.
-    /// </summary>
-    public const string CreateQrcIdReservation = "PluginsSbp/CreateQrcIdReservation";
+        /// <summary>
+        /// Получение идентификаторов для многоразовых ссылок СБП.
+        /// </summary>
+        public const string CreateQrcIdReservation = "PluginsSbp/CreateQrcIdReservation";
 
-    /// <summary>
-    /// Регистрация Кассовой ссылки СБП.
-    /// </summary>
-    public const string СreateCashRegisterQr = "PluginsSbp/СreateCashRegisterQr";
+        /// <summary>
+        /// Регистрация Кассовой ссылки СБП.
+        /// </summary>
+        public const string СreateCashRegisterQr = "PluginsSbp/СreateCashRegisterQr";
 
-    /// <summary>
-    /// Активация Кассовой ссылки СБП.
-    /// </summary>
-    public const string ActivateCashRegisterQr = "PluginsSbp/ActivateCashRegisterQr";
+        /// <summary>
+        /// Активация Кассовой ссылки СБП.
+        /// </summary>
+        public const string ActivateCashRegisterQr = "PluginsSbp/ActivateCashRegisterQr";
 
-    /// <summary>
-    /// Деактивация Кассовой ссылки СБП.
-    /// </summary>
-    public const string DeactivateCashRegisterQr = "PluginsSbp/DeactivateCashRegisterQr";
+        /// <summary>
+        /// Деактивация Кассовой ссылки СБП.
+        /// </summary>
+        public const string DeactivateCashRegisterQr = "PluginsSbp/DeactivateCashRegisterQr";
 
-    /// <summary>
-    /// Запрос статуса Операций СБП по идентификаторам QR. 
-    /// </summary>
-    public const string GetStatusQrcOperations = "PluginsSbp/GetStatusQrcOperations";
+        /// <summary>
+        /// Запрос статуса Операций СБП по идентификаторам QR. 
+        /// </summary>
+        public const string GetStatusQrcOperations = "PluginsSbp/GetStatusQrcOperations";
 
-    /// <summary>
-    /// Запрос Агента ТСП на возврат по Операции СБП C2B.
-    /// </summary>
-    public const string CreatePaymentPetition = "PluginsSbp/CreatePaymentPetition";
+        /// <summary>
+        /// Запрос Агента ТСП на возврат по Операции СБП C2B.
+        /// </summary>
+        public const string CreatePaymentPetition = "PluginsSbp/CreatePaymentPetition";
 
-    /// <summary>
-    /// Статус запроса на возврат средств для Агента ТСП.
-    /// </summary>
-    public const string GetRefundStatus = "PluginsSbp/GetRefundStatus";
+        /// <summary>
+        /// Статус запроса на возврат средств для Агента ТСП.
+        /// </summary>
+        public const string GetRefundStatus = "PluginsSbp/GetRefundStatus";
+    }
 }
